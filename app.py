186d50b1dc93248ad2ea83ff23c85577f58b7189
Andres Wilches <andresfwilchest@gmail.com>
2022-02-18 20:59:33 -0500

    added something that doesn't funct 1

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 from flask import Flask, escape,render_template, request
 from flask_wtf import CSRFProtect
 from flask_mail import Mail,Message
-import forms
+import forms, config
+
 
 ##  Global
 app = Flask(__name__)
@@ -21,19 +22,23 @@ def index():
 #path vista agregar estudiante
 @app.route('/addStudent')
 def addStudent():
-    return render_template('addStudent.html')
+    auditionForm = forms.AuditionForm()
+    return render_template('addStudent.html', form = auditionForm)
 
 #path para insertar persona estudiante audicion
-@app.route('/succesStudent/<registro>',methods=['GET','POST'])
-def insertStudentfAudition(registro):
-    audition_form = forms.CreateForm(request.form)
+@app.route('/successStudent',methods=['GET','POST'])
+def insertStudentfAudition():
+    print('prueba '+request.form['email'])
+    audition_form = forms.AuditionForm(request.form)
     if request.method == 'POST' and audition_form.validate():
-        print(audition_form.email.data)
+        print('Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+        print('correo registrado: '+audition_form.email.data)
         ## email sending
         msg = Message('TeatrosUD: Agendacion audicion',
         sender = app.config['MAIL_USERNAME'],
         recipients=[audition_form.email.data])
-    return render_template('successInsStud.html', registro=registro)
+
+    return render_template('successIns.html')
 
 #path vista ver estudiante
 @app.route('/viewStudent')
