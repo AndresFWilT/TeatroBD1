92bc49db3ff27ad68a0c09b71dcaddbf48f02edd
Josue Nuñez P <josuenunezprada@gmail.com>
2022-02-19 18:55:19 -0500

    Add querys for insert county, play, typeplay

--- queries.sql
+++ queries.sql
@@ -1,17 +1,17 @@
 
 -- Isertar perona
 INSERT INTO person 
-VALUES (1321651, 'Juan', 'Bermudez', 'C.C', 'juan@email.com', to_date('31/10/2007','DD/MM/YYYY'))
+VALUES (1321651, 'Juan', 'Bermudez', 'C.C', 'juan@email.com', to_date('15/08/1996','DD/MM/YYYY'))
 
 -- Isertar estudiante
-INSERT INTO student VALUES (20161020065, 1321651, 'Ing. Sistemas');
+INSERT INTO student VALUES (20161020065, 1321651, 'Ing. Sistemas')
 
 -- Obtener el ultimo registro
-SELECT * FROM audition WHERE idaudition=(SELECT max(idaudition) FROM audition);
+SELECT * FROM audition WHERE idaudition=(SELECT max(idaudition) FROM audition)
 
 -- Insertar audicion
-INSERT INTO audition (code, idplay, dateaudition) 
-VALUES (1321651, 1, timestamp '20/02/2022 08:00','DD/MM/YYYY HH:MI');
+INSERT INTO audition (idnumber, code, idplay, dateaudition) 
+VALUES (1321651, 20161020065, 1, to_date('2022/02/22 08:00:00', 'yyyy/mm/dd hh24:mi:ss'))
 
 -- Obtener los datos de un estudiante
 SELECT P.names, P.surname, S.code, S.career FROM person P, student S WHERE P.idnumber = S.idnumber 
@@ -20,3 +20,13 @@ SELECT P.names, P.surname, S.code, S.career FROM person P, student S WHERE P.idn
 SELECT PE.names, PL.title, A.code, A.dateaudition
 FROM person PE, play PL, audition A, student S
 WHERE PE.idnumber = S.idnumber and S.code = A.code and A.idplay = PL.idplay
+
+-- Insertar Pais
+INSERT INTO country VALUES (44, 'United Kingdom')
+
+-- Insertar tipos de obra
+INSERT INTO typeplay (nametypeplay) VALUES ('Melodrama')
+
+-- Insertar una obra
+INSERT INTO play (idtypeplay, countrycode, title, releasedate) 
+VALUES (1, 44, 'Romeo y Julieta', to_date('31/10/1597','DD/MM/YYYY'))
