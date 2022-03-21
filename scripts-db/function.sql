2c06469f64fb2e70f72825ee8698204be280b745
crisfelipe22 <cris_ovalles@hotmail.com>
2022-03-20 21:07:46 -0500

    Scripts.db:update Faltan functions

---
+++ scripts-db/function.sql
@@ -0,0 +1,5 @@
+/*1.7. Incluir el calendario de la obra que quedo activa: incluir 1 convocatoria, 6 ensayos 
+y 3 funciones
+ID_PLAY, ID_FUNCTION, ID_TYPE_MODALITY, THEATER_CODE, FUNCTION_DATE, START_TIME, END_TIME
+*/
+INSERT INTO function VALUES ('RADJ',0001,'CV','TLF',to_date('25/03/2022','DD/MM/YYYY'),to_date('08:00:00', 'hh24:mi:ss'),to_date('10:00:00', 'hh24:mi:ss'));
