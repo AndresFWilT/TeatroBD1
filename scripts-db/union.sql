1ddf234118781959ff56997e891abeb96e875060
crisfelipe22 <cris_ovalles@hotmail.com>
2022-03-27 23:48:12 -0500

    SelectAttendace

--- scripts-db/union.sql
+++ scripts-db/union.sql
@@ -257,22 +257,22 @@ END_CHARACTER_DATE */
 INSERT INTO character_student (id_play, id_character, student_code, start_character_date, 
                                end_character_date) 
 VALUES ('RADJ', 'RMMT', '20172020071', to_date('28/02/2022','DD/MM/YYYY'), 
-        to_date('22/04/2022','DD/MM/YYYY'));
+        to_date('22/03/2022','DD/MM/YYYY'));
 
 INSERT INTO character_student (id_play, id_character, student_code, start_character_date, 
                                end_character_date) 
 VALUES ('RADJ', 'FTLZ', '20172020068', to_date('13/02/2022','DD/MM/YYYY'), 
-        to_date('22/04/2022','DD/MM/YYYY'));
+        to_date('22/03/2022','DD/MM/YYYY'));
 
 INSERT INTO character_student (id_play, id_character, student_code, start_character_date, 
                                end_character_date) 
 VALUES ('RADJ', 'MDEL', '20172020114', to_date('23/02/2022','DD/MM/YYYY'), 
-        to_date('22/04/2022','DD/MM/YYYY'));
+        to_date('22/03/2022','DD/MM/YYYY'));
 
 INSERT INTO character_student (id_play, id_character, student_code, start_character_date, 
                                end_character_date) 
 VALUES ('RADJ', 'JTCT', '20171010058', to_date('18/02/2022','DD/MM/YYYY'), 
-        to_date('22/04/2022','DD/MM/YYYY'));
+        to_date('22/03/2022','DD/MM/YYYY'));
 
 INSERT INTO character_student (id_play, id_character, student_code, start_character_date, 
                                end_character_date) 
@@ -612,22 +612,22 @@ END_CHARACTER_DATE */
 INSERT INTO character_student (id_play, id_character, student_code, start_character_date, 
                                end_character_date) 
 VALUES ('RADJ', 'RMMT', '20172020071', to_date('28/02/2022','DD/MM/YYYY'), 
-        to_date('22/04/2022','DD/MM/YYYY'));
+        to_date('22/03/2022','DD/MM/YYYY'));
 
 INSERT INTO character_student (id_play, id_character, student_code, start_character_date, 
                                end_character_date) 
 VALUES ('RADJ', 'FTLZ', '20172020068', to_date('13/02/2022','DD/MM/YYYY'), 
-        to_date('22/04/2022','DD/MM/YYYY'));
+        to_date('22/03/2022','DD/MM/YYYY'));
 
 INSERT INTO character_student (id_play, id_character, student_code, start_character_date, 
                                end_character_date) 
 VALUES ('RADJ', 'MDEL', '20172020114', to_date('23/02/2022','DD/MM/YYYY'), 
-        to_date('22/04/2022','DD/MM/YYYY'));
+        to_date('22/03/2022','DD/MM/YYYY'));
 
 INSERT INTO character_student (id_play, id_character, student_code, start_character_date, 
                                end_character_date) 
 VALUES ('RADJ', 'JTCT', '20171010058', to_date('18/02/2022','DD/MM/YYYY'), 
-        to_date('22/04/2022','DD/MM/YYYY'));
+        to_date('22/03/2022','DD/MM/YYYY'));
 
 
 INSERT INTO character_student (id_play, id_character, student_code, start_character_date, 
