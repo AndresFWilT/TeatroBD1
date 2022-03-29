dabd6cb7f999964d5ccc874f59aa3103e73441a1
crisfelipe22 <cris_ovalles@hotmail.com>
2022-03-29 01:28:46 -0500

    Tables_EL,WPS,SPS

--- scripts-db/stage_play_staff.sql
+++ scripts-db/stage_play_staff.sql
@@ -5,3 +5,5 @@ VALUES ('IGNST','SNOD',1,'RADJ',to_date('25/03/2022','DD/MM/YYYY'),to_date('06/0
 INSERT INTO STAGE_PLAY_STAFF (UNIT_CODE, EMPLOYEE_CODE, ID_ROLE, ID_PLAY, STAFF_START_DATE, STAFF_END_DATE) 
 VALUES ('IGNST','AJDZ',1,'RADJ',to_date('25/03/2022','DD/MM/YYYY'),to_date('06/05/2022 20:00', 'DD/MM/YYYY hh24:mi'));                                                                                      
 
+INSERT INTO STAGE_PLAY_STAFF (UNIT_CODE, EMPLOYEE_CODE, ID_ROLE, ID_PLAY, STAFF_START_DATE, STAFF_END_DATE) 
+VALUES ('FCATS','RNQT',2,'RADJ',to_date('25/03/2022','DD/MM/YYYY'),to_date('06/05/2022 20:00', 'DD/MM/YYYY hh24:mi'));                                                                                      
